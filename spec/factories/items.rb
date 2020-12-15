b6f54d19a3e2c00a5b20467f675ce4d61e697a2b
Shaun James <ShaunDaneJames@Gmail.com>
2020-12-15 16:23:02 -0700

    Removes merchant id from item factory, these are created int the tests

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,6 +3,5 @@ FactoryBot.define do
     name { Faker::Games::Pokemon.name }
     description { Faker::Games::Pokemon.move }
     unit_price { Faker::Number.decimal(l_digits: 3, r_digits: 2) }
-    merchant { Faker::Games::Zelda.character }
   end
 end
