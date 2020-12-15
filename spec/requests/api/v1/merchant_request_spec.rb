0f2bfe8c363ec39aa8303cb4ba5ecb5a05c073bb
Shaun James <ShaunDaneJames@Gmail.com>
2020-12-15 16:26:15 -0700

    Alters test to expect serialized data

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -4,9 +4,20 @@ describe 'merchant API' do
   it 'sends a list of merchants' do
     create_list(:merchant, 3)
 
-    get '/api/v1/merchants'
+    get api_v1_merchants_path
+
     expect(response).to be_successful
 
-    merchants = JSON.parse(response.body)
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants[:data].count).to eq(3)
+
+    merchants.each do |merchant|
+      expect(merchants[:data][0][:attributes]).to have_key(:id)
+      expect(merchants[:data][0][:attributes][:id]).to be_an(Integer)
+
+      expect(merchants[:data][0][:attributes]).to have_key(:name)
+      expect(merchants[:data][0][:attributes][:name]).to be_a(String)
+    end
   end
 end
