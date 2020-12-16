a13a298a110a451dae945d975419a7d112f6dbf0
Shaun James <ShaunDaneJames@Gmail.com>
2020-12-15 20:15:49 -0700

    Adds update action

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -7,4 +7,11 @@ class Api::V1::MerchantsController < ApplicationController
   def show
     render json: MerchantSerializer.new(Merchant.find(params[:id]))
   end
+
+  def update
+    render json: MerchantSerializer.new(Merchant.find(params[:id]))
+    merchant = Merchant.find(params[:id])
+    merchant.update({name: params[:name]})
+    merchant.save
+  end
 end
