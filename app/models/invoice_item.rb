dd0c028cf8dbf27178dc9a0c91b66332af466f5a
Shaun James <ShaunDaneJames@Gmail.com>
2020-12-16 14:23:21 -0700

    Updates validations and relationships

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,8 +2,10 @@ class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
 
-  validates_presence_of :item
-  validates_presence_of :invoice
+  has_many :transactions, through: :invoice
+  
+  # validates_presence_of :item
+  # validates_presence_of :invoice
   validates_presence_of :quantity
   validates_presence_of :unit_price
 end
