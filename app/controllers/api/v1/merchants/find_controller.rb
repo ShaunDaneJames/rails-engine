0c97d398521dd8860f1dbeda1b94ccc366faaecb
Shaun James <ShaunDaneJames@Gmail.com>
2020-12-17 14:28:00 -0700

    Adds show and index methods for merchants find controller

--- app/controllers/api/v1/merchants/find_controller.rb
+++ app/controllers/api/v1/merchants/find_controller.rb
@@ -4,4 +4,9 @@ class Api::V1::Merchants::FindController < ApplicationController
     render json: MerchantSerializer.new(
       Merchant.where("lower(name) LIKE ?", "%" + params[:name].downcase + "%").first)
   end
+
+  def index
+    render json: MerchantSerializer.new(
+      Merchant.where("lower(name) LIKE ?", "%" + params[:name].downcase + "%"))
+  end
 end
