eb7f9135342cebe3d4e9b933e8735e9e23d1bc26
Shaun James <ShaunDaneJames@Gmail.com>
2020-12-17 11:30:57 -0700

    Adds route for find all items

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,14 @@ Rails.application.routes.draw do
 
       namespace :items do
         get '/:id/merchants', to: 'search#show'
+        get '/find', to: 'find#show'
+        get '/find_all', to: 'find#index'
       end
       resources :items
 
       namespace :merchants do
         get '/:id/items', to: 'search#index'
+        get '/find', to: 'find#show'
       end
       resources :merchants
     end
