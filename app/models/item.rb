c1c25e8666e7bf16279b800c6262946a83c42452
Shaun James <ShaunDaneJames@Gmail.com>
2020-12-17 18:18:41 -0700

    Cleans up white space and removes not used method

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,8 +9,4 @@ class Item < ApplicationRecord
   validates_presence_of :description
   validates_presence_of :unit_price
   validates_presence_of :merchant_id
-
-  def fetch(item)
-    item.downcase
-  end
 end
