d992816565e4b56015d8d27e9e782371f57dcea3
Shaun James <ShaunDaneJames@Gmail.com>
2020-12-17 18:17:00 -0700

    Creates most revenue method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,4 +5,13 @@ class Merchant < ApplicationRecord
   has_many :transactions, through: :invoices
 
   validates_presence_of :name
+
+  def self.most_revenue(limit)
+    select("merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+    .joins(invoices: [:invoice_items, :transactions])
+    .merge(Transaction.successful)
+    .group(:id)
+    .order("revenue DESC")
+    .limit(limit)
+  end
 end
